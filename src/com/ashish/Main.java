e4d4295ea128a106c14e118de84e534f09417350
averma111 <ashish.etl@outlook.com>
2020-04-16 17:34:10 -0700

    String Data Type Exmaple for JAVA

---
+++ src/com/ashish/Main.java
@@ -0,0 +1,23 @@
+package com.ashish;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String myString = "This is the string example";
+        System.out.println(myString);
+        myString =  myString + ",this more example of string";
+        System.out.println(myString);
+
+        String lastString = "10";
+        int myInt = 50;
+        lastString =  lastString + myInt;
+        System.out.println("LastString is =" + lastString);
+
+        double doubleNumber = 158.36d;
+        lastString = lastString + doubleNumber;
+
+        System.out.println(lastString:
+        );
+    }
+}
